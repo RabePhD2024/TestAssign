2a1018a538598405a68953a0f0da828626c8a9a0
RabePhD2024 <167175771+RabePhD2024@users.noreply.github.com>
2024-05-02 09:24:16 +0200

    Working Model
    After running it over night

--- Assignment/ObjectiveFunction.m
+++ Assignment/ObjectiveFunction.m
@@ -4,36 +4,23 @@ ReturnValOnError = 10000000.0;
 PenaltyFactor = 1000.0;
 
 % Write the file with b-spline parameters
-fp = fopen('InputData.any','w+');
+fp = fopen('C:\Users\raeann\Downloads\AnyBody Folder\Assignment/InputData.any','w+');
 fprintf(fp,'AnyFloat DriverData = {{');
 for i=1:(length(x)/2-1)
   fprintf(fp,'%.13f,',x(i));
 end
 fprintf(fp,'%.13f},{',x(length(x)/2));
+
+
+fprintf(fp,'AnyFloat DriverData = {{');
 for i=length(x)/2+1:(length(x)-1)
   fprintf(fp,'%.13f,',x(i));
 end
-fprintf(fp,'%.13f},{',x(length(x)));
-
-for i=length(x)+1:length(x)+length(x)/2-1
-  fprintf(fp,'0.0,');
-end
-fprintf(fp,'0.0}};');
+fprintf(fp,'%.13f},{',x(end));
 
 fclose(fp);
 
-fprintf(fp,'%.13f,',x(1)); % Write Hip angle
-fprintf(fp,'}}; \n ');
-
-
-
-fprintf(fp,'AnyFloat DriverData_Knee = {{');
-fprintf(fp,'%.13f,',x(2)); % Write Hip angle
-fprintf(fp,'}};');
-fclose(fp);
-
-
-% % Execute the model
+ % Execute the model
 [status,result] = system(['"C:\Program Files\AnyBody Technology\AnyBody.8.0\AnyBodyCon.exe" /m ExeMacro.anymcr']);
 
 % check for error
@@ -44,14 +31,65 @@ if error_check > 0
     return;
 end
 
-Data = readABO('../Output.txt');
+Data = readABO('C:\Users\raeann\Downloads\AnyBody Folder\Assignment/Output.txt');
 
 t = cell2mat(Data.Values(1));
-Foot_x=cell2mat(Data.Values(2));
-Foot_y=cell2mat(Data.Values(3));
-Vel = cell2mat(Data.Values(4));
+FootPosX=cell2mat(Data.Values(2));
+FootVelX=cell2mat(Data.Values(5));
+MaxMusAct=cell2mat(Data.Values(8));
+HipAngle=cell2mat(Data.Values(9));
+KneeAngle = cell2mat(Data.Values(10));
+
+
+% further joint boundaries
+% We set the Joint angle parameters as follows
+MaxHipAngle = 120;
+MinHipAngle = -30;
+MaxKneeAngle = 0;
+MinKneeAngle = -150;
+
 
 % objective function
-f = -max(Vel);
+f = 0;
+MaximumVel=0;
+AnyArray_Temproray= zeros(6,1);
+
+for j=2:length(t)
+    if FootPosX(j)>0 && FootPosX(j+1)<0           
+        f=-max(FootVelX(j-1));
+    else
+        f=ReturnValOnError;
+    end
 end
+
+% further joint boundaries
+% We set the Joint angle parameters as follows
+HipFlexion = 120;
+HipExtension = 30;
+KneeFlexion = 150
+KneeExtension= 0;
+
+
+if min(HipAngle)<-HipFlexion*(pi/180)
+    f = f + PenaltyFactor*(-min(HipAngle)^2);
+end
+
+if max(HipAngle)>HipExtension*(pi/180)
+    f = f + PenaltyFactor*(-max(HipAngle)^2);
+end 
+
+if min(KneeAngle)<-KneeFlexion*(pi/180)
+    f = f + PenaltyFactor*(min(KneeAngle)^2);
+end 
+
+if max(KneeAngle)>KneeExtension
+    f = f + PenaltyFactor*(max(KneeAngle)^2);
+end 
+
+
+if max(MaxMusAct)>1
+    f = f + PenaltyFactor*max(MaxMusAct);
+end
+
+
 
--- Assignment/OptimizeMain.m
+++ Assignment/OptimizeMain.m
@@ -1,23 +1,36 @@
 clear all;
 close all;
+MaxHipAngle = 120;
+MinHipAngle = -30;
+MaxKneeAngle = 0;
+MinKneeAngle = -150;
 
 addpath('readABO');
 addpath('ComplexOptimiser');
 
 % Initial guess of the design variables
-xInit = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
+xInit = [-25*(pi/180),-20*(pi/180),-15*(pi/180),-10*(pi/180),-5*(pi/180),0*(pi/180),5*(pi/180),10*(pi/180),15*(pi/180),20*(pi/180),-115*(pi/180),-90*(pi/180),-65*(pi/180),-40*(pi/180),-15*(pi/180),0*(pi/180),0*(pi/180),0*(pi/180),0*(pi/180),0*(pi/180),0*(pi/180),0*(pi/180)];
 
 Options.m = length(xInit); % Number of design variables
-Options.n = 2*Options.m+1; % Number of elements in the population. 2*m+1 is minimum. Higher is recommended
+Options.n = 10*Options.m+1; % Number of elements in the population. 2*m+1 is minimum. Higher is recommended
 
-LowerBoxLimit = -1.0; % Lower bound on the box that the initial poputation is randomly created within 
-UpperBoxLimit = 1.0; % Upper bound on the box that the initial poputation is randomly created within
+LowerBoxLimit_HipBoundaries = -MaxHipAngle*(pi/180); % Lower bound on the box that the initial poputation is randomly created within 
+UpperBoxLimit_HipBoundaries = -MinHipAngle*(pi/180); % Upper bound on the box that the initial poputation is randomly created within
+
+LowerBoxLimit_KneeBoundaries = MinKneeAngle*(pi/180); % Lower bound on the box that the initial poputation is randomly created within 
+UpperBoxLimit_KneeBoundaries = MaxKneeAngle*(pi/180); % Upper bound on the box that the initial poputation is randomly created within
 
 % The same upper bound is used for all design variables. If it is desired
 % to different bounds for each design variable, this must be changed.
-for i=1:length(xInit)
-    Options.BoxSize(i,1) = LowerBoxLimit;
-    Options.BoxSize(i,2) = UpperBoxLimit;
+
+for i = 1:length(xInit)
+    if i <= length(xInit)/2
+        Options.BoxSize(i,1) = LowerBoxLimit_HipBoundaries;
+        Options.BoxSize(i,2) = UpperBoxLimit_HipBoundaries;
+    else
+        Options.BoxSize(i,1) = LowerBoxLimit_KneeBoundaries;
+        Options.BoxSize(i,2) = UpperBoxLimit_KneeBoundaries;
+    end
 end
 
 Options.convergence.criteria = 0.001*ones(length(xInit),1); % Convergence criteria for the design variables.
@@ -25,6 +38,6 @@ Options.convergence.criteria = 0.001*ones(length(xInit),1); % Convergence criter
 Options.n_r = 4; % Number of repetitions a new solutions is allowed to remain the worst before it is replaced with a new random point
 Options.k = 1.3; % the distance projected accross the centroid. 1.3 is recommended.
 
-Options.MaxSteps = 100; % Maximum number of iterations
+Options.MaxSteps = 10000; % Maximum number of iterations
 
 [qOpt,fOpt, exitflag] = ComplexOptimiser(@(x) ObjectiveFunction(x),xInit,[],Options)
